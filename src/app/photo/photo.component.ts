439275e60ae584a68d209cdbe079312de80d0d7b
Deyveson Willian <deyvesonn@outlook.com>
2018-10-28 23:35:34 -0400

    Inbound properties
    passando dados para o componente

--- src/app/photo/photo.component.ts
+++ src/app/photo/photo.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Input } from '@angular/core';
 
 @Component({
   selector: 'app-photo',
@@ -7,7 +7,7 @@ import { Component, OnInit } from '@angular/core';
 })
 export class PhotoComponent {
 
-  description = 'Leão';
-  url = 'https://cdn.pixabay.com/photo/2018/07/31/22/08/lion-3576045__340.jpg';
+  @Input()  description = '';
+  @Input()  url = '';
 
 }
