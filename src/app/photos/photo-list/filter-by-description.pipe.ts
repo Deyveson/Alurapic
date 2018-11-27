45c75e8c3084d64daec5a38dbc1edd55bea8d25f
deyveson <deyvesonn@outlook.com>
2018-11-27 16:24:13 -0400

    Pipe e implementação

---
+++ src/app/photos/photo-list/filter-by-description.pipe.ts
@@ -0,0 +1,20 @@
+import {Pipe, PipeTransform} from '@angular/core';
+
+@Pipe({name:'filterByDescription'})
+export class FilterByDescription implements PipeTransform{
+  transform(photos: Photo[], descriptionQuery: string){
+    descriptionQuery = descriptionQuery
+      .trim()
+      .toLowerCase();
+
+    if (descriptionQuery) {
+      return photos.filter(photo =>
+        photo.description.toLowerCase().includes(descriptionQuery)
+      );
+    } else {
+      return photos;
+    }
+    // Pega essa lista de Photo, pra cada photo pega a description que ela tem, joga pra lowercase, e ve nessa string tem o que digitei.
+    // se tem ele entra na lista do filter,
+  }
+}
