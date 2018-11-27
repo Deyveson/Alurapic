d3a87e88dd6b6922e191b25f6956c40e76127978
deyveson <deyvesonn@outlook.com>
2018-11-27 15:26:30 -0400

    Change Detection

--- src/app/photos/photo-list/photos/photos.component.ts
+++ src/app/photos/photo-list/photos/photos.component.ts
@@ -1,4 +1,4 @@
-import {Component, Input, OnInit} from '@angular/core';
+import {Component, Input, OnChanges, SimpleChanges} from '@angular/core';
 import {Photo} from '../../photo/photo';
 
 @Component({
@@ -6,13 +6,25 @@ import {Photo} from '../../photo/photo';
   templateUrl: './photos.component.html',
   styleUrls: ['./photos.component.css']
 })
-export class PhotosComponent implements OnInit {
+export class PhotosComponent implements OnChanges {
 
   @Input() photos: Photo[] = [];
+  rows: any[] = [];
 
   constructor() { }
 
-  ngOnInit() {
+  groupColumns(photos: Photo[]){
+    const newRows = [];
+    for(let index = 0; index < photos.length; index+=3){
+      newRows.push(photos.slice(index, index + 3))
+    }
+    // O primeiro parâmetro de slice é a posição inclusive na qual os elementos serão considerados. O segundo é a posição final (não inclusiva).
+    return newRows;
+  }
+
+  ngOnChanges(changes: SimpleChanges): void {
+    if(changes.photos)
+      this.rows = this.groupColumns(this.photos);
   }
 
 }
