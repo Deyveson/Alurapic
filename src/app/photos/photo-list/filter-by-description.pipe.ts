ba5356e759481f8ffbbca3ec81083209d5bde1bf
deyveson <deyvesonn@outlook.com>
2018-11-28 15:51:37 -0400

    Resolvers
    Resolver, capaz de lidar com dados durante a navegação de uma rota para disponibilizá-los a um componente antes deste ser carregado.

--- src/app/photos/photo-list/filter-by-description.pipe.ts
+++ src/app/photos/photo-list/filter-by-description.pipe.ts
@@ -1,6 +1,7 @@
 import {Pipe, PipeTransform} from '@angular/core';
+import {Photo} from '../photo/photo';
 
-@Pipe({name:'filterByDescription'})
+@Pipe({name: 'filterByDescription'})
 export class FilterByDescription implements PipeTransform{
   transform(photos: Photo[], descriptionQuery: string){
     descriptionQuery = descriptionQuery
--- src/app/photos/photo-list/photo-list.component.ts
+++ src/app/photos/photo-list/photo-list.component.ts
@@ -1,6 +1,5 @@
 import { Component, OnInit } from '@angular/core';
 import {Photo} from '../photo/photo';
-import {PhotoService} from '../photo/photo.service';
 import {ActivatedRoute} from '@angular/router';
 
 @Component({
@@ -15,16 +14,9 @@ export class PhotoListComponent implements OnInit {
 
   filter: string = '';
 
-  constructor (
-    private photoService: PhotoService,
-    private activatedRoute: ActivatedRoute
-  ) {}
+  constructor(private activatedRoute: ActivatedRoute) {}
 
   ngOnInit(): void {
-    const userName = this.activatedRoute.snapshot.params.userName;
-    this.photoService
-      .listFromUser(userName)
-      .subscribe(photos => this.photos = photos);
+    this.photos = this.activatedRoute.snapshot.data['photos'];
   }
-
 }
