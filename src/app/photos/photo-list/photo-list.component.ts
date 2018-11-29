0c5b37313648ef0ee9bcfa99fffb3d143fbc211c
deyveson <deyvesonn@outlook.com>
2018-11-29 07:42:28 -0400

    RxJS e seu Subject
    Aplicando debounce, debounceTime e ngOnDestroy para liberar o Subject evitando gastos desnecessários da memória.

--- src/app/photos/photo-list/photo-list.component.ts
+++ src/app/photos/photo-list/photo-list.component.ts
@@ -1,22 +1,31 @@
-import { Component, OnInit } from '@angular/core';
-import {Photo} from '../photo/photo';
+import {Component, OnDestroy, OnInit} from '@angular/core';
 import {ActivatedRoute} from '@angular/router';
+import {Subject} from 'rxjs';
+import {debounceTime} from 'rxjs/operators';
+
+import {Photo} from '../photo/photo';
 
 @Component({
   selector: 'app-photo-list',
   templateUrl: './photo-list.component.html',
   styleUrls: ['./photo-list.component.css']
 })
-export class PhotoListComponent implements OnInit {
+export class PhotoListComponent implements OnInit, OnDestroy {
 
   photos: Photo [] = [];
-  // declaro a variavel com o tipo 'Object [] = [];'
-
   filter: string = '';
+  debounce: Subject<string> = new Subject<string>();
 
   constructor(private activatedRoute: ActivatedRoute) {}
 
   ngOnInit(): void {
     this.photos = this.activatedRoute.snapshot.data['photos'];
+    this.debounce
+      .pipe(debounceTime(300))
+      .subscribe(filter => this.filter = filter);
+  }
+
+  ngOnDestroy(): void {
+    this.debounce.unsubscribe();
   }
 }
