89da9febcce9d01e5e8770306d61a3b87be52f65
deyveson <deyvesonn@outlook.com>
2018-11-29 09:00:53 -0400

    Submódulos
    Organizando a aplicação, por modulos

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,12 +1,11 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-import { AppComponent } from './app.component';
+// BOA PRATICA Primeiro os importes do angular
 
+import { AppComponent } from './app.component';
 import { PhotosModule } from './photos/photos.module';
 import {AppRoutingModule} from './app.routing.module';
 import {ErrorsModule} from './errors/errors.module';
-// BOA PRATICA Primeiro os importes do angular
-
 // Segundo os importes feitos por mim
 
 @NgModule({
