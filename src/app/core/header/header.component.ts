656d2e3b545ea4fe5f3cdf1008f44b19153bbb9c
deyveson <deyvesonn@outlook.com>
2018-12-03 14:55:42 -0400

    O pipe async

--- src/app/core/header/header.component.ts
+++ src/app/core/header/header.component.ts
@@ -11,10 +11,8 @@ import {User} from '../user/user';
 export class HeaderComponent {
 
   user$: Observable<User>;
-  user: User;
 
-  constructor(private userService: UserService ) {
+  constructor(userService: UserService) {
     this.user$ = userService.getUser();
-    this.user$.subscribe(user => this.user = user);
   }
 }
